31ca3216795bb5d565cc416d2570a72309b92faf
hi-rev <hirev05@gmail.com>
2023-03-08 14:11:42 +0900

    jsx tutorials #2

---
+++ 1.basics/ex08/src/04/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+    // return (
+    //     <p>함수 컴포넌트 만들기</p>
+    // );
+
+    return React.createElement('p', null, '함수 컴포넌트 만들기');
+}
+
+export default Content;
---
+++ 1.basics/ex08/src/07/Content.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import Clock from './Clock';
+
+
+function Content() {
+    return (
+        <Fragment>
+            <p>{'Dynamic HTML Rendering'}</p>
+            <Clock />
+        </Fragment>
+    );
+}
+
+export default Content;
