e8eaf140419bb64f884debd71b0497fc8e4b34cf
hi-rev <hirev05@gmail.com>
2023-03-17 13:31:34 +0900

    case sensitive

--- kanbanboard/frontend/config/webpack.config.js
+++ kanbanboard/frontend/config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = function(env){
     return {
@@ -33,6 +34,9 @@ module.exports = function(env){
             }]
         },
         devtool: "eval-source-map",
+        plugins: [
+            new CaseSensitivePathsPlugin()
+        ],
         devServer: {
             host: '0.0.0.0',
             port: 9090,
